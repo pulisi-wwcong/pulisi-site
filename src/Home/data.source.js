51a9618366a714fcc0f9e486de3297d0b944ed0c
wuwencong <wwc@flyrise.cn>
2022-09-06 01:41:28 +0800

    init

---
+++ src/Home/data.source.js
@@ -0,0 +1,479 @@
+import React from 'react';
+export const Nav30DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header3-logo',
+    children:
+      'https://gw.alipayobjects.com/zos/basement_prod/b30cdc2a-d91c-4c78-be9c-7c63b308d4b3.svg',
+  },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航一', name: 'text' }],
+        },
+        subItem: [
+          {
+            name: 'sub0',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+          {
+            name: 'sub1',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: 'item1',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航二', name: 'text' }],
+        },
+      },
+      {
+        name: 'item2',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航三', name: 'text' }],
+        },
+      },
+      {
+        name: 'item3',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [{ children: '导航四', name: 'text' }],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Banner10DataSource = {
+  wrapper: { className: 'banner1' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg0' },
+        title: {
+          className: 'banner1-title',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+        },
+        content: {
+          className: 'banner1-content',
+          children: '一个高效的页面动画解决方案',
+        },
+        button: { className: 'banner1-button', children: 'Learn More' },
+      },
+      {
+        name: 'elem1',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg1' },
+        title: {
+          className: 'banner1-title',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+        },
+        content: {
+          className: 'banner1-content',
+          children: '一个高效的页面动画解决方案',
+        },
+        button: { className: 'banner1-button', children: 'Learn More' },
+      },
+      {
+        name: 'elem2',
+        BannerElement: { className: 'banner-user-elem' },
+        textWrapper: { className: 'banner1-text-wrapper' },
+        bg: { className: 'bg bg1' },
+        title: {
+          className: 'banner1-title',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
+        },
+        content: {
+          className: 'banner1-content',
+          children: '一个高效的页面动画解决方案',
+        },
+        button: { className: 'banner1-button', children: 'Learn More' },
+      },
+    ],
+  },
+};
+export const Feature70DataSource = {
+  wrapper: { className: 'home-page-wrapper feature7-wrapper' },
+  page: { className: 'home-page feature7' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'feature7-title-wrapper',
+    children: [
+      {
+        name: 'title',
+        className: 'feature7-title-h1',
+        children: '图像在线服务',
+      },
+      {
+        name: 'content',
+        className: 'feature7-title-content',
+        children: '你可以直接快速接入图像能力',
+      },
+    ],
+  },
+  blockWrapper: {
+    className: 'feature7-block-wrapper',
+    gutter: 24,
+    children: [
+      {
+        md: 6,
+        xs: 24,
+        name: 'block0',
+        className: 'feature7-block',
+        children: {
+          className: 'feature7-block-group',
+          children: [
+            {
+              name: 'image',
+              className: 'feature7-block-image',
+              children:
+                'https://gw.alipayobjects.com/zos/basement_prod/e339fc34-b022-4cde-9607-675ca9e05231.svg',
+            },
+            {
+              name: 'title',
+              className: 'feature7-block-title',
+              children: '身份证',
+            },
+            {
+              name: 'content',
+              className: 'feature7-block-content',
+              children: '识别身份证正反面姓名、身份证号、发证机关等相关信息',
+            },
+          ],
+        },
+      },
+      {
+        md: 6,
+        xs: 24,
+        name: 'block1',
+        className: 'feature7-block',
+        children: {
+          className: 'feature7-block-group',
+          children: [
+            {
+              name: 'image',
+              className: 'feature7-block-image',
+              children:
+                'https://gw.alipayobjects.com/zos/basement_prod/e339fc34-b022-4cde-9607-675ca9e05231.svg',
+            },
+            {
+              name: 'title',
+              className: 'feature7-block-title',
+              children: '身份证',
+            },
+            {
+              name: 'content',
+              className: 'feature7-block-content',
+              children: '识别身份证正反面姓名、身份证号、发证机关等相关信息',
+            },
+          ],
+        },
+      },
+      {
+        md: 6,
+        xs: 24,
+        name: 'block2',
+        className: 'feature7-block',
+        children: {
+          className: 'feature7-block-group',
+          children: [
+            {
+              name: 'image',
+              className: 'feature7-block-image',
+              children:
+                'https://gw.alipayobjects.com/zos/basement_prod/e339fc34-b022-4cde-9607-675ca9e05231.svg',
+            },
+            {
+              name: 'title',
+              className: 'feature7-block-title',
+              children: '身份证',
+            },
+            {
+              name: 'content',
+              className: 'feature7-block-content',
+              children: '识别身份证正反面姓名、身份证号、发证机关等相关信息',
+            },
+          ],
+        },
+      },
+      {
+        md: 6,
+        xs: 24,
+        name: 'block3',
+        className: 'feature7-block',
+        children: {
+          className: 'feature7-block-group',
+          children: [
+            {
+              name: 'image',
+              className: 'feature7-block-image',
+              children:
+                'https://gw.alipayobjects.com/zos/basement_prod/e339fc34-b022-4cde-9607-675ca9e05231.svg',
+            },
+            {
+              name: 'title',
+              className: 'feature7-block-title',
+              children: '身份证',
+            },
+            {
+              name: 'content',
+              className: 'feature7-block-content',
+              children: '识别身份证正反面姓名、身份证号、发证机关等相关信息',
+            },
+          ],
+        },
+      },
+      {
+        md: 6,
+        xs: 24,
+        name: 'block4',
+        className: 'feature7-block',
+        children: {
+          className: 'feature7-block-group',
+          children: [
+            {
+              name: 'image',
+              className: 'feature7-block-image',
+              children:
+                'https://gw.alipayobjects.com/zos/basement_prod/e339fc34-b022-4cde-9607-675ca9e05231.svg',
+            },
+            {
+              name: 'title',
+              className: 'feature7-block-title',
+              children: '身份证',
+            },
+            {
+              name: 'content',
+              className: 'feature7-block-content',
+              children: '识别身份证正反面姓名、身份证号、发证机关等相关信息',
+            },
+          ],
+        },
+      },
+      {
+        md: 6,
+        xs: 24,
+        name: 'block5',
+        className: 'feature7-block',
+        children: {
+          className: 'feature7-block-group',
+          children: [
+            {
+              name: 'image',
+              className: 'feature7-block-image',
+              children:
+                'https://gw.alipayobjects.com/zos/basement_prod/e339fc34-b022-4cde-9607-675ca9e05231.svg',
+            },
+            {
+              name: 'title',
+              className: 'feature7-block-title',
+              children: '身份证',
+            },
+            {
+              name: 'content',
+              className: 'feature7-block-content',
+              children: '识别身份证正反面姓名、身份证号、发证机关等相关信息',
+            },
+          ],
+        },
+      },
+      {
+        md: 6,
+        xs: 24,
+        name: 'block6',
+        className: 'feature7-block',
+        children: {
+          className: 'feature7-block-group',
+          children: [
+            {
+              name: 'image',
+              className: 'feature7-block-image',
+              children:
+                'https://gw.alipayobjects.com/zos/basement_prod/e339fc34-b022-4cde-9607-675ca9e05231.svg',
+            },
+            {
+              name: 'title',
+              className: 'feature7-block-title',
+              children: '身份证',
+            },
+            {
+              name: 'content',
+              className: 'feature7-block-content',
+              children: '识别身份证正反面姓名、身份证号、发证机关等相关信息',
+            },
+          ],
+        },
+      },
+      {
+        md: 6,
+        xs: 24,
+        name: 'block7',
+        className: 'feature7-block',
+        children: {
+          className: 'feature7-block-group',
+          children: [
+            {
+              name: 'image',
+              className: 'feature7-block-image',
+              children:
+                'https://gw.alipayobjects.com/zos/basement_prod/e339fc34-b022-4cde-9607-675ca9e05231.svg',
+            },
+            {
+              name: 'title',
+              className: 'feature7-block-title',
+              children: '身份证',
+            },
+            {
+              name: 'content',
+              className: 'feature7-block-content',
+              children: '识别身份证正反面姓名、身份证号、发证机关等相关信息',
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Footer10DataSource = {
+  wrapper: { className: 'home-page-wrapper footer1-wrapper' },
+  OverPack: { className: 'footer1', playScale: 0.2 },
+  block: {
+    className: 'home-page',
+    gutter: 0,
+    children: [
+      {
+        name: 'block0',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          className: 'logo',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/qqaimmXZVSwAhpL.svg',
+        },
+        childWrapper: {
+          className: 'slogan',
+          children: [
+            {
+              name: 'content0',
+              children: 'Animation specification and components of Ant Design.',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: '产品' },
+        childWrapper: {
+          children: [
+            { name: 'link0', href: '#', children: '产品更新记录' },
+            { name: 'link1', href: '#', children: 'API文档' },
+            { name: 'link2', href: '#', children: '快速入门' },
+            { name: 'link3', href: '#', children: '参考指南' },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: '关于' },
+        childWrapper: {
+          children: [
+            { href: '#', name: 'link0', children: 'FAQ' },
+            { href: '#', name: 'link1', children: '联系我们' },
+          ],
+        },
+      },
+      {
+        name: 'block3',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: { children: '资源' },
+        childWrapper: {
+          children: [
+            { href: '#', name: 'link0', children: 'Ant Design' },
+            { href: '#', name: 'link1', children: 'Ant Motion' },
+          ],
+        },
+      },
+    ],
+  },
+  copyrightWrapper: { className: 'copyright-wrapper' },
+  copyrightPage: { className: 'home-page' },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        ©2018 by <a href="https://motion.ant.design">Ant Motion</a> All Rights
+        Reserved
+      </span>
+    ),
+  },
+};
